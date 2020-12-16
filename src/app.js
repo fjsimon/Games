820aec77d5cdd276795422b3558c58760a2067ff
fjsimon <fjlopez.mail@gmail.com>
2020-12-16 10:31:40 +0000

    Update navigation. Remove unused views.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ export class App {
     config.map([
       { route: '',            name: 'sudoku',      moduleId: './routes/sudoku/sudoku',              nav: true, title: 'Sudoku' },
       { route: 'eightQueens', name: 'eightQueens', moduleId: './routes/eight-queens/eight-queens',  nav: true, title: 'Eight Queens' },
-      { route: 'connectFour', name: 'connectFour', moduleId: './routes/connect-four/connect-four',  nav: true, title: 'Connect Four' },
-      { route: 'simon',       name: 'simon',       moduleId: './routes/simon',                      nav: true, title: 'Simon says' }
+      { route: 'connectFour', name: 'connectFour', moduleId: './routes/connect-four/connect-four',  nav: true, title: 'Connect Four' }
     ]);
 
     this.router = router;
